a5b8bb21f58fc9c024b62b1dc5a9d9528c512c6f
Caio99BR <caiooliveirafarias0@gmail.com>
2015-03-26 12:15:18 -0300

    moved

--- vee3/vee3-vendor-blobs.mk
+++ vee3/vee3-vendor-blobs.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/lge/vee3
+LOCAL_PATH := vendor/lge-vee3/vee3
 
 # Binaries
 PRODUCT_COPY_FILES += \
