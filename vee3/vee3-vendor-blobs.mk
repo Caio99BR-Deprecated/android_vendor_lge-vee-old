400767da7cc732d51f85cbba9f4ede79445ced6a
Caio99BR <caiooliveirafarias0@gmail.com>
2015-04-05 22:36:32 -0300

    Add camera blob
    This reverts commit a9c7a432ab6873cd66c80cbd241ec9fe202e710d.

--- vee3/vee3-vendor-blobs.mk
+++ vee3/vee3-vendor-blobs.mk
@@ -50,6 +50,7 @@ PRODUCT_COPY_FILES += \
 # HW
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so \
+	$(LOCAL_PATH)/proprietary/lib/hw/camera.msm7x27a.so:system/lib/hw/camera.msm7x27a.so \
 
 # Modules
 PRODUCT_COPY_FILES += \
